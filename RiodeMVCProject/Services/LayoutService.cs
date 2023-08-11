4e5e1331b12051663b50a0a6a5c1a74b810d1d33
Adil Tehranli <adiljt@code.edu.az>
2023-08-11 16:40:10 +0400

    Updated AdminLayout

--- RiodeMVCProject/Services/LayoutService.cs
+++ RiodeMVCProject/Services/LayoutService.cs
@@ -1,6 +1,16 @@
-﻿namespace RiodeMVCProject.Services
+﻿using Microsoft.EntityFrameworkCore;
+using RiodeMVCProject.DataAccess;
+
+namespace RiodeMVCProject.Services;
+
+public class LayoutService
 {
-    public class LayoutService
+    readonly RiodeDbContext _context;
+
+    public LayoutService(RiodeDbContext context)
     {
+        _context = context;
     }
+    public async Task<Dictionary<string, string>> GetSettings()
+ => await _context.Settings.ToDictionaryAsync(s => s.Key, s => s.Value);
 }
