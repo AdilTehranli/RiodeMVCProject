66454dc58b9bd1b9b69b63eaf75b691e3b21779f
Adil Tehranli <adiljt@code.edu.az>
2023-08-13 23:17:41 +0400

    Updated product

--- RiodeMVCProject/Controllers/HomeController.cs
+++ RiodeMVCProject/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RiodeMVCProject.DataAccess;
+using RiodeMVCProject.Services.Interfaces;
 using RiodeMVCProject.ViewModels.HomeVMs;
 
 namespace RiodeMVCProject.Controllers
@@ -8,17 +9,18 @@ namespace RiodeMVCProject.Controllers
     public class HomeController : Controller
     {
         readonly RiodeDbContext _context;
-
-        public HomeController(RiodeDbContext context)
+        readonly ISliderService _sliderService;
+        public HomeController(RiodeDbContext context, ISliderService sliderService)
         {
             _context = context;
+            _sliderService = sliderService;
         }
 
         public async Task<IActionResult>  Index()
         {
             HomeVM vm = new HomeVM()
             {
-                Sliders = await _context.Sliders.ToListAsync(),
+                Sliders = await _sliderService.GetAll(),
                 Banners=await _context.Banners.ToListAsync(),
                 
             };
